5676812ec4bc716fabcf99b6ae2681832505ea9a
ghostman2311 <78917858+ghostman2311@users.noreply.github.com>
2023-05-01 09:29:30 +0000

    Implemented authorization using jwt

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router = express.Router()
 const { signup, signin} = require('../auth.controller');
+const verifyToken = require('../middleware/authJWT');
 
 router.post("/register", signup, (req, res) => {});
 router.post("/login", signin, (req, res) => {});
+
+router.get('/hiddencontent', verifyToken, (req, res) => {
+    if(!req.user){
+        res.status(403).send({message: "Invalid JWT token"})
+    }
+
+    if(req.user.role === "admin"){
+        res.status(200).send({message: "Congratulation but there is no hidden content"})
+    } else {
+        res.status(403).send({message: "Unauthorized Access"})
+    }
+})
 
 module.exports = router;
